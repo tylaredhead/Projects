1d987984e1c3243790676aee537cc63105ac284b
Tyla Redhead <TKR499@student.bham.ac.uk>
2025-08-19 18:47:09 +0100

    remove vulnerability

--- InventoryManagement/frontend/inventmanagment/src/components/inputbox.js
+++ InventoryManagement/frontend/inventmanagment/src/components/inputbox.js
@@ -5,9 +5,8 @@ function Inputbox(props){
 
     return (
         <div classStyle = 'container'>
-            <label for="props.type">props.type</label>
-            <input type="{ props.type }" id="{ props.type }" placeholder="{ props.text} "></input>
-        
+            <label for="{ props.type }">{ props.type }</label>
+            <input type="{ props.type }" id="{ props.type }" placeholder="{ props.text }" required></input>
         </div>
     );
 }
