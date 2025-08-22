43c5df105ed697636c69b1f9b80c7ede5cadab96
Tyla Redhead <TKR499@student.bham.ac.uk>
2025-08-22 22:51:59 +0100

    Adding toggle button for password inputbox

--- InventoryManagement/frontend/inventorymanagement/src/components/pages/Login.jsx
+++ InventoryManagement/frontend/inventorymanagement/src/components/pages/Login.jsx
@@ -6,6 +6,7 @@ function Login() {
     const [password, setpassword] = useState("");
     const [invalidLogin, setinvalidLogin] = useState(false);
     const [showToggle, setshowToggle] = useState(false);
+    const [isLoading, setisLoading] = useState(false);
 
     const updateusername = (s) => {setusername(s.target.value);};
     const updatepassword = (s) => {setpassword(s.target.value);};
@@ -18,6 +19,7 @@ function Login() {
                 <h1>Login</h1>
                 <form onSubmit={(e) => {
                     <h1>Hi</h1>
+                    //setisLoading(false);
                 }}>
                     <label for='username'>Username:</label>
                     <input type='text' id='username' name='username' placeholder='Enter your username...' onChange={updateusername} required />
@@ -26,7 +28,9 @@ function Login() {
                         <input className='password-input' type={(showToggle) ? 'text' : 'password'} id='password' name='password' placeholder='Enter your password...' onChange={updatepassword} required />
                         <img src={(!showToggle) ? '/public/showToggle.svg' : '/public/hideToggle.svg'} alt='show toggle password' onClick={handleToggle}></img>
                     </div>
-                    <button className='button-events' type='submit'>Log in</button>
+                    <button className='button-events' type='submit'>
+                        Log in
+                    </button>
                     { invalidLogin && <p className = 'error-msg'>Your username or password entered is invalid, try again!</p>}
                     <p>Can't login? Contact admin for an account</p>
                 </form>
